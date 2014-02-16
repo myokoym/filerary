3bf6217bb714091bbceb5052b892df8d67e77f74
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-16 20:00:02 +0900

    Remove meaningless sample

--- sample/tutorial.rb
+++
@@ -1,9 +0,0 @@
-require "filerary"
-
-filerary = Filerary::Librarian.new
-
-base_dir = File.join(File.dirname(__FILE__), "..")
-filerary.collect(Dir.glob("#{base_dir}/*"))
-
-p filerary.search("require")
-p filerary.search("Windows")
