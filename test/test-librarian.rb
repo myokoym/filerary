65489e2fa286564f21be55b7e0ca0d42908ed5df
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-16 16:47:40 +0900

    test: fix a variable name

--- test/test-librarian.rb
+++ test/test-librarian.rb
@@ -2,20 +2,20 @@ require "filerary"
 
 class FileraryTest < Test::Unit::TestCase
   def setup
-    @filerary = Filerary::Librarian.new
+    @librarian = Filerary::Librarian.new
   end
 
   def test_default_db_dir
     home_dir = File.expand_path("~")
     default_db_dir = File.join(home_dir, ".filerary", "db")
-    assert_equal(default_db_dir, @filerary.db_dir)
+    assert_equal(default_db_dir, @librarian.db_dir)
   end
 
   def test_collect
-    assert_true(@filerary.respond_to?(:collect))
+    assert_true(@librarian.respond_to?(:collect))
   end
 
   def test_search
-    assert_true(@filerary.respond_to?(:search))
+    assert_true(@librarian.respond_to?(:search))
   end
 end
