aa2b4919eec4c323b83e7678c00f971b432dd649
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-16 16:53:19 +0900

    test: use temporary directory for testing

--- test/test-librarian.rb
+++ test/test-librarian.rb
@@ -1,14 +1,25 @@
 require "filerary"
+require "fileutils"
 
 class FileraryTest < Test::Unit::TestCase
   def setup
-    @librarian = Filerary::Librarian.new
+    @test_base_dir = File.join(File.dirname(__FILE__), "tmp")
+    FileUtils.mkdir_p(@test_base_dir)
+    @librarian = Filerary::Librarian.new(@test_base_dir)
+  end
+
+  def teardown
+    Dir.glob("#{@test_base_dir}/*.db*") do |path|
+      FileUtils.rm(path)
+    end
+    FileUtils.rmdir(@test_base_dir)
   end
 
   def test_default_db_dir
     home_dir = File.expand_path("~")
+    librarian = Filerary::Librarian.new
     default_db_dir = File.join(home_dir, ".filerary", "db")
-    assert_equal(default_db_dir, @librarian.db_dir)
+    assert_equal(default_db_dir, librarian.db_dir)
   end
 
   def test_collect
